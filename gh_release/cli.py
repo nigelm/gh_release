44d2e9525e6e71ef2eaf313d1fa2df83fa4f1757
Nigel Metheringham <nigel.metheringham@gmail.com>
2022-01-26 20:39:20 +0000

    fix: correct tests version checking

--- gh_release/cli.py
+++ gh_release/cli.py
@@ -1,10 +1,12 @@
 """Console script for gh_release."""
 import click
 
+from . import __version__
+
 
 # -----------------------------------------------------------------------
 @click.command()
-@click.version_option()
+@click.version_option(version=__version__)
 def gh_release():
     """gh_release dummy command"""
     print("Hello world")
